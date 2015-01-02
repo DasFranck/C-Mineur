8421a361ff2f3108cc723fac92f3bb25d9c0e456
DasArch <dasfranck@gmx.com>
2015-01-02 18:42:19 +0000

    rm func.*wq

--- CLI-Clock/func.c
+++
@@ -1,9 +0,0 @@
-#include "func.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-void printhelp()
-{
-    printf("Soon here : An help page\n");
-}
-
--- CLI-Clock/func.h
+++
@@ -1,5 +0,0 @@
-/* -f = format (change time & date format)
- * -h = printhelp (print the help page)
- */
-
-void printhelp();
